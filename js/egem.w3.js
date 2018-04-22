9598b0846317904d8874d22f578e31d5e3d55948
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-04-22 13:45:49 -0700

    Bitshares assets added to tools page for tracking.

--- js/egem.w3.js
+++ js/egem.w3.js
@@ -8,6 +8,8 @@ var balance1 = web3.fromWei(web3.eth.getBalance("0xfc0f0a5F06cB00c9EB435127142ac
 var balance2 = web3.fromWei(web3.eth.getBalance("0x0666bf13ab1902de7dee4f8193c819118d7e21a6"), "ether");
 var balance3 = web3.fromWei(web3.eth.getBalance("0xcEf0890408b4FC0DC025c8F581c77383529D38B6"), "ether");
 
+var balance4 = web3.fromWei(web3.eth.getBalance("0x5488f1d22a47fee54cf77a36caa899c9db28b513"), "ether");
+
 // Get balance of the address entered
 function qBalance() {
     var x = document.querySelector('[name="addressInput"]').value;
@@ -37,10 +39,12 @@ function qDevsBalance() {
   var balance1 = web3.fromWei(web3.eth.getBalance("0xfc0f0a5F06cB00c9EB435127142ac79ac6F48B94"), "ether");
   var balance2 = web3.fromWei(web3.eth.getBalance("0x0666bf13ab1902de7dee4f8193c819118d7e21a6"), "ether");
   var balance3 = web3.fromWei(web3.eth.getBalance("0xcEf0890408b4FC0DC025c8F581c77383529D38B6"), "ether");
+  var balance4 = web3.fromWei(web3.eth.getBalance("0x5488f1d22a47fee54cf77a36caa899c9db28b513"), "ether");
     document.getElementById("bal0").innerText = balance0
     document.getElementById("bal1").innerText = balance1
     document.getElementById("bal2").innerText = balance2
     document.getElementById("bal3").innerText = balance3
+    document.getElementById("bal4").innerText = balance4
 }
 
 function lBlock() {
