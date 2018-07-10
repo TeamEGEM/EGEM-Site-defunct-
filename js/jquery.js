fcdab2680ec030f6728de28c9fbce6e82e7d0338
Derek May <32908855+riddlez666@users.noreply.github.com>
2018-07-10 04:26:12 -0700

    unhide menu

--- js/jquery.js
+++ js/jquery.js
@@ -6,7 +6,6 @@ $(function() {
       $( ".friends-area" ).fadeToggle( "slow", "linear" );
     });
 
-    $( ".quickm-area" ).hide();
     $( "#qmopen" ).click(function() {
       $( ".quickm-area" ).fadeToggle( "slow", "linear" );
     });
